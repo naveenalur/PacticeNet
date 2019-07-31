4192a9153c231fc4ac9e863177b2df2dfed98984
INDIENCE\naveena <naveena.am@unvired.io>
2019-07-31 16:56:06 +0530

    Binary search

--- BinarySearchAlogorothem/BinarySearch.cs
+++ BinarySearchAlogorothem/BinarySearch.cs
@@ -10,16 +10,20 @@ namespace BinarySearchAlogorothem
 
             PrintArray(SortArray(array));
 
-
             int poistion = BinarySearchFindKey(0, array.Length, 14, SortArray(array));
-            Console.WriteLine("The element present at " + poistion + "\n");
+            if (poistion != -1)
+                Console.WriteLine("The element present at " + poistion + "\n");
+
+            Console.WriteLine("Using recursion ");
+            int poistion1 = BinarySearchByRecursion(0, array.Length, 2, SortArray(array));
+            if (poistion1 != -1)
+                Console.WriteLine("The element present at " + poistion1 + "\n");
+
             Console.ReadLine();
         }
 
-
         public static int BinarySearchFindKey(int low, int high, int key, int[] array)
         {
-
             while (low <= high)
             {
                 int mid = (low + high) / 2;
@@ -36,17 +40,30 @@ namespace BinarySearchAlogorothem
                 {
                     return mid;
                 }
-
-
             }
             return -1;
-
         }
 
-
+        public static int BinarySearchByRecursion(int low, int high, int key, int[] array)
+        {
+            if (high >= low)
+            {
+                int mid = (low + high) / 2;
+                if (array[mid] == key)
+                    return mid;
+                else if (array[mid] > key)
+                {
+                    return BinarySearchByRecursion(low, mid - 1, key, array);
+                }
+                else if (array[mid] < key)
+                {
+                    return BinarySearchByRecursion(mid + 1, high, key, array);
+                }
+            }
+            return -1;
+        }
         public static int[] SortArray(int[] array)
         {
-
             for (int i = 0; i < array.Length - 1; i++)
             {
                 for (int j = 0; j < array.Length - i - 1; j++)
@@ -61,7 +78,6 @@ namespace BinarySearchAlogorothem
             }
             return array;
         }
-
 
         public static void PrintArray(int[] array)
         {
